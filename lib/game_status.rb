567191e8d1b82671a98d4b95b3e94938364f1a32
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-10 02:37:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,11 +17,11 @@ WIN_COMBINATIONS = [
   
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combo|
-    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo{2]]) == "X"
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
       return win_combo
-    elsifif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo{2]]) == "O"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
       return win_combo
-    else
-      FALSE
+    end
+      false
   end
 end
