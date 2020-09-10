5ad8f746cf79aedc9eaa9fb3c5653869b9a56cde
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-10 02:57:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,7 +35,7 @@ def draw?(board)
 end
 
 def over?(board)
-  !(won?(board)) && (full?(board)) && (draw?(board))
+  if !(won?(board)) && (full?(board)) && (draw?(board))
     return true
   else
     false
